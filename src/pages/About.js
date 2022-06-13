d175a1d53ec1e9a6be8276bc61af6057f8a0f7c6
Kruthi Thangali <kruthi.thangali@gmail.com>
2022-06-13 15:41:04 -0700

    final submission

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+
+    import IMG_9183 from "../assets/IMG_9183.jpg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <p><div>
+        <div class="split left">
+          <div className="centered">
+            <img 
+              className="profile_image"
+              src= {IMG_9183}
+              alt="photo"
+              width="430"
+              height="600"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Kruthi!</div>
+            <div className="brief_description">
+              I'm a junior at Carnegie Mellon University. 
+              I study statistics and machine learning.
+              I like singing and baking.
+            </div>
+          </div>
+        </div>
+      </div> </p> 
       </div>
     )
   }
